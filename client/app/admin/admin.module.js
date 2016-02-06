92838f7972ee1ed0a93f4c15e538718e31c21001
buzzlockinc <buzzlockinc@gmail.com>
2016-02-05 18:10:00 -0800

    First Commit

---
+++ client/app/admin/admin.module.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('estimateApp.admin', [
+  'estimateApp.auth',
+  'ui.router'
+]);
---
+++ client/app/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('estimateApp', [
+  'estimateApp.auth',
+  'estimateApp.admin',
+  'estimateApp.constants',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'btford.socket-io',
+  'ui.router',
+  'ui.bootstrap',
+  'validation.match'
+])
+  .config(function($urlRouterProvider, $locationProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
